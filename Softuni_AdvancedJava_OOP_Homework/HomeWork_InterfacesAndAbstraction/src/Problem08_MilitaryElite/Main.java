cddbc8d361c82c06d0abe6ffa2763ca39ca00503
tishkata86 <tishkata86@gmail.com>
2016-07-17 15:10:16 +0300

    do some changes

--- Softuni_AdvancedJava_OOP_Homework/HomeWork_InterfacesAndAbstraction/src/Problem08_MilitaryElite/Main.java
+++ Softuni_AdvancedJava_OOP_Homework/HomeWork_InterfacesAndAbstraction/src/Problem08_MilitaryElite/Main.java
@@ -1,11 +1,6 @@
 package Problem08_MilitaryElite;
 
-import Problem08_MilitaryElite.Models.Private;
-import Problem08_MilitaryElite.Models.PrivateModels.LeutenantGeneral;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
