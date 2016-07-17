21352c850e4a8509b6cec232033276e6fda63230
tishkata86 <tishkata86@gmail.com>
2016-07-17 22:58:32 +0300

    do some changes

---
+++ Softuni_AdvancedJava_OOP_Homework/HomeWork_InterfacesAndAbstraction/src/Problem09_CollectionHierarchy/Main.java
@@ -0,0 +1,12 @@
+package Problem09_CollectionHierarchy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
