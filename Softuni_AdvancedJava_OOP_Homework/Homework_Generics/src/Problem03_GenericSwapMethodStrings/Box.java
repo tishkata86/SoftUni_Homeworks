0cebae65d7827bd364b51c60beef3444bbee20d0
tishkata86 <tishkata86@gmail.com>
2016-07-18 23:42:15 +0300

    adding generic homework - problem03

---
+++ Softuni_AdvancedJava_OOP_Homework/Homework_Generics/src/Problem03_GenericSwapMethodStrings/Box.java
@@ -0,0 +1,22 @@
+package Problem03_GenericSwapMethodStrings;
+
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.setValue(value);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    private void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value.getClass().getTypeName() + ": " + value;
+    }
+}
