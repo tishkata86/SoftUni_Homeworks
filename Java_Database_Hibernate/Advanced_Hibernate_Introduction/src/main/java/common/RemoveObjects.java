469e8f522140390cab5ae4f501d3f841beb8bd07
Tihomir Dobrev <tishkata86@gmail.com>
2017-06-21 12:23:38 +0300

    add file

--- Java_Database_Hibernate/Advanced_Hibernate_Introduction/src/main/java/common/RemoveObjects.java
+++ Java_Database_Hibernate/Advanced_Hibernate_Introduction/src/main/java/common/RemoveObjects.java
@@ -1,8 +1,13 @@
 package common;
 
+import entities.Town;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RemoveObjects {
 
@@ -13,7 +18,24 @@ public class RemoveObjects {
 
         em.getTransaction().begin();
 
+        List<Town> towns = new ArrayList<>();
 
+        Query query = em.createNativeQuery("SELECT * FROM towns");
+        List<Object[]> nativeQuery = query.getResultList();
+
+        for (Object[] objects : nativeQuery) {
+            int pk = (int) objects[0];
+            Town town = em.find(Town.class, pk);
+            towns.add(town);
+            if (town.getName().length() > 5){
+                em.detach(town);
+            } else {
+                town.setName(town.getName().toLowerCase());
+                em.merge(town);
+            }
+        }
+
+        System.out.println(towns);
 
         em.getTransaction().commit();
         em.close();
